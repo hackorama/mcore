f866a261a758d9db586e76cfe63ea2a8bff2f329
hackorama <kishan.thomas@gmail.com>
2019-01-24 15:12:30 -0800

    Adding MySQL test for JDBC data store

---
+++ src/test/java/com/hackorama/mcore/data/MySQLTest.java
@@ -0,0 +1,37 @@
+package com.hackorama.mcore.data;
+
+import static org.junit.Assert.assertEquals;
+
+import java.net.HttpURLConnection;
+import java.sql.SQLException;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import com.hackorama.mcore.data.jdbc.JDBCDataStore;
+import com.hackorama.mcore.server.spark.SparkServer;
+import com.hackorama.mcore.service.Service;
+import com.hackorama.mcore.service.group.GroupService;
+
+public class MySQLTest {
+
+    private static final String DEFAULT_SERVER_ENDPOINT = "http://127.0.0.1:8080" ;
+
+    public static void main(String[] args) throws SQLException, UnirestException {
+        if(args.length < 1) {
+            System.out.println("Usage: java MySQLTest <db_password>");
+            System.exit(1);
+        }
+        Service service = new GroupService().configureUsing(new JDBCDataStore("jdbc:mysql://localhost/test",
+                "com.mysql.cj.jdbc.Driver", "test", args[0])).configureUsing(new SparkServer("test")).start();
+        HttpResponse<JsonNode> response = Unirest.post(DEFAULT_SERVER_ENDPOINT + "/group")
+                .header("accept", "application/json").body("{ \"name\" : \"one\" }").asJson();
+        assertEquals(HttpURLConnection.HTTP_OK, response.getStatus());
+        response = Unirest.get(DEFAULT_SERVER_ENDPOINT + "/group").header("accept", "application/json").asJson();
+        System.out.println(response.getBody().toString());
+        service.stop();
+    }
+
+}
