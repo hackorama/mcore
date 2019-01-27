096bf73fa3636030a96e1f64f5aa73fbb0a81bc7
hackorama <kishan.thomas@gmail.com>
2019-01-26 17:46:36 -0800

    Cleaning up test server data from test

--- src/test/java/com/hackorama/mcore/data/MySQLTest.java
+++ src/test/java/com/hackorama/mcore/data/MySQLTest.java
@@ -28,22 +28,20 @@ public class MySQLTest {
     private static final String DEFAULT_SERVER_ENDPOINT = "http://127.0.0.1:8080" ;
 
     public static void main(String[] args) throws SQLException, UnirestException {
-        if(args.length < 1) {
-            System.out.println("Usage: java MySQLTest <db_password>");
-            //System.exit(1);
-        }
-        Service service = new GroupService().configureUsing(new JDBCDataStore("jdbc:mysql://localhost/test",
-              "com.mysql.cj.jdbc.Driver", "test", args[0])).configureUsing(new SparkServer("test")).start();
-        //Service service = new GroupService().configureUsing(new JDBCDataStore("jdbc:mysql://sql3.freesqldatabase.com/sql3275761",
-         //      "com.mysql.cj.jdbc.Driver", "sql3275761", "hIvqjGH5wx")).configureUsing(new SparkServer("test")).start();
-        //Service service = new GroupService().configureUsing(new JDBCDataStore("jdbc:postgresql://baasu.db.elephantsql.com:5432/godwbhlk",
-         //     "org.postgresql.Driver", "godwbhlk", "syVFX7pI-cnqIfq_8ZYK3dCVTIEbN6e8")).configureUsing(new SparkServer("test")).start();
-        HttpResponse<JsonNode> response = Unirest.post(DEFAULT_SERVER_ENDPOINT + "/group")
-                .header("accept", "application/json").body("{ \"name\" : \"one\" }").asJson();
-        assertEquals(HttpURLConnection.HTTP_OK, response.getStatus());
-        response = Unirest.get(DEFAULT_SERVER_ENDPOINT + "/group").header("accept", "application/json").asJson();
-        System.out.println(response.getBody().toString());
-        service.stop();
-    }
+		if (args.length < 1) {
+			System.out.println("Usage: java MySQLTest <db_password>");
+			System.exit(1);
+		}
+		Service service = new GroupService()
+				.configureUsing(
+						new JDBCDataStore("jdbc:mysql://localhost/test", "com.mysql.cj.jdbc.Driver", "test", args[0]))
+				.configureUsing(new SparkServer("test")).start();
+		HttpResponse<JsonNode> response = Unirest.post(DEFAULT_SERVER_ENDPOINT + "/group")
+				.header("accept", "application/json").body("{ \"name\" : \"one\" }").asJson();
+		assertEquals(HttpURLConnection.HTTP_OK, response.getStatus());
+		response = Unirest.get(DEFAULT_SERVER_ENDPOINT + "/group").header("accept", "application/json").asJson();
+		System.out.println(response.getBody().toString());
+		service.stop();
+	}
 
 }
