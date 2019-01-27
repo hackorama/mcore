1ad76a804e061d3b0895bb8b1a50c86cbfc8b565
hackorama <kishan.thomas@gmail.com>
2019-01-26 20:20:16 -0800

    Adding place holders for additional HSQL, Derby, Sqlite tests

---
+++ src/test/java/com/hackorama/mcore/data/JDBCMemoryHSQLDataStoreTest.java
@@ -0,0 +1,11 @@
+package com.hackorama.mcore.data;
+
+import java.sql.SQLException;
+
+public class JDBCMemoryHSQLDataStoreTest {
+
+    protected void createDataStore() throws SQLException {
+        //dataStore = new JDBCDataStore("jdbc:hsqldb:mem");
+    }
+
+}
---
+++ src/test/java/com/hackorama/mcore/data/JDBCMemorySqliteDataStoreTest.java
@@ -0,0 +1,13 @@
+package com.hackorama.mcore.data;
+
+import java.sql.SQLException;
+
+
+public class JDBCMemorySqliteDataStoreTest {
+
+    //@Override
+    protected void createDataStore() throws SQLException {
+        //dataStore = new JDBCDataStore("jdbc:sqlite:");
+    }
+
+}
