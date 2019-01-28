395421692791ecc195bce35f0704a2c57a7d0fc7
hackorama <kishan.thomas@gmail.com>
2019-01-27 19:13:10 -0800

    Adding data message queue interface

---
+++ src/main/java/com/hackorama/mcore/data/queue/DataQueue.java
@@ -0,0 +1,12 @@
+package com.hackorama.mcore.data.queue;
+
+import java.util.function.Function;
+
+
+public interface DataQueue {
+
+    public void publish(String channel, String message);
+
+    public void consume(String channel, Function<String, Boolean> handler);
+
+}
