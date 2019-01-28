688a1266a00a678463c83d4bcd764d4d3385663f
hackorama <kishan.thomas@gmail.com>
2019-01-27 19:15:30 -0800

    Updating service interface to support injecting cache and queue

--- src/main/java/com/hackorama/mcore/service/Service.java
+++ src/main/java/com/hackorama/mcore/service/Service.java
@@ -1,12 +1,24 @@
 package com.hackorama.mcore.service;
 
 import com.hackorama.mcore.data.DataStore;
+import com.hackorama.mcore.data.cache.DataCache;
+import com.hackorama.mcore.data.queue.DataQueue;
 import com.hackorama.mcore.server.Server;
 
 public interface Service {
     public Service attach(Service service);
 
-    public Service configureUsing(DataStore dataStore);
+    public default Service configureUsing(DataCache dataCache) {
+        return this;
+    }
+
+    public default Service configureUsing(DataQueue dataQueue) {
+        return this;
+    }
+
+    public default Service configureUsing(DataStore dataStore) {
+        return this;
+    }
 
     public Service configureUsing(Server server);
 
