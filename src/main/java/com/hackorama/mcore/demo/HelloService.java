c678592e22a5930dc3550fe58b573b51c6672a2d
hackorama <kishan.thomas@gmail.com>
2019-06-04 17:28:58 -0700

    Updating using base service

--- src/main/java/com/hackorama/mcore/demo/HelloService.java
+++ src/main/java/com/hackorama/mcore/demo/HelloService.java
@@ -5,28 +5,9 @@ import com.google.gson.Gson;
 import com.hackorama.mcore.common.HttpMethod;
 import com.hackorama.mcore.common.Request;
 import com.hackorama.mcore.common.Response;
-import com.hackorama.mcore.server.Server;
-import com.hackorama.mcore.service.Service;
+import com.hackorama.mcore.service.BaseService;
 
-public class HelloService implements Service {
-
-    private static Server server;
-    @Override
-    public Service attach(Service service) {
-        return null;
-    }
-
-    private static void setServer(Server server) {
-        HelloService.server = server;
-    }
-
-    @Override
-    public Service configureUsing(Server server) {
-        HelloService.setServer(server);
-        server.setRoutes(HttpMethod.GET, "/hello", HelloService::getHello);
-        server.setRoutes(HttpMethod.GET, "/hello/{name}", HelloService::getHello);
-        return this;
-    }
+public class HelloService extends BaseService {
 
     public static Response getHello(Request request) {
         System.out.println(request.getParams());
@@ -39,16 +20,9 @@ public class HelloService implements Service {
     }
 
     @Override
-    public Service start() {
-        server.start();
-        return this;
-    }
-
-    @Override
-    public void stop() {
-        if(server != null) {
-            server.stop();
-        }
+    public void configure() {
+        server.setRoutes(HttpMethod.GET, "/hello", HelloService::getHello);
+        server.setRoutes(HttpMethod.GET, "/hello/{name}", HelloService::getHello);
     }
 
 }
