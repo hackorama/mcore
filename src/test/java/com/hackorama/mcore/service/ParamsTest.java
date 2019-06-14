c1bf8c449051ddf1f43a4bf701ab202ba2c39f11
hackorama <kishan.thomas@gmail.com>
2019-06-13 21:02:23 -0700

    Adding tests for parameter handling

---
+++ src/test/java/com/hackorama/mcore/service/ParamsTest.java
@@ -0,0 +1,78 @@
+package com.hackorama.mcore.service;
+
+import static org.junit.Assert.*;
+
+import java.net.HttpURLConnection;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import com.hackorama.mcore.common.HttpMethod;
+import com.hackorama.mcore.common.Request;
+import com.hackorama.mcore.common.Response;
+import com.hackorama.mcore.common.TestServer;
+
+@RunWith(Parameterized.class)
+public class ParamsTest {
+
+    private static class TestParams extends BaseService {
+
+        public static Response getParams(Request request) {
+            boolean result = "one".equals(request.getParams().get("one"));
+            result = "two".equals(request.getParams().get("two"));
+            result = "three".equals(request.getParams().get("three"));
+            return new Response("ONE", result ? HttpURLConnection.HTTP_OK : HttpURLConnection.HTTP_BAD_REQUEST);
+        }
+
+        @Override
+        public void configure() {
+            route(HttpMethod.GET, "/test/{one}/{two}/{three}", TestParams::getParams);
+            route(HttpMethod.GET, "/test/{one}/{two}/{three}/", TestParams::getParams);
+        }
+    }
+
+    @AfterClass
+    public static void afterAllTests() throws Exception {
+        TestServer.awaitShutdown();
+    }
+
+    @Parameters
+    public static Iterable<? extends Object> data() {
+        return TestServer.getServerTypeList();
+    }
+
+    public ParamsTest(String serverType) {
+        TestServer.setServerType(serverType);
+    }
+
+    @Test
+    public void service_attachServicesUnderSameServer_expectsNoErrors() throws UnirestException {
+        new TestParams().configureUsing(TestServer.createNewServer()).start();
+        TestServer.awaitStartup();
+        assertTrue(TestServer.validResponseCode("/test/one/two/three", HttpURLConnection.HTTP_OK));
+        assertTrue(TestServer.validResponseCode("/test/one/two/three/", HttpURLConnection.HTTP_OK));
+        assertFalse(TestServer.validResponseCode("/test/one/two/three/x", HttpURLConnection.HTTP_OK));
+        assertFalse(TestServer.validResponseCode("/test/two/three", HttpURLConnection.HTTP_OK));
+        assertFalse(TestServer.validResponseCode("/test/two/three/one", HttpURLConnection.HTTP_OK));
+        assertFalse(TestServer.validResponseCode("/test/three/one/two", HttpURLConnection.HTTP_OK));
+        TestServer.awaitShutdown();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("Testing with server type: " + TestServer.getServerType());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        TestServer.awaitShutdown();
+    }
+
+}
