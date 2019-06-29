b6de97339eea5a55691318c96705c422e791e035
hackorama <kishan.thomas@gmail.com>
2019-06-28 18:50:20 -0700

    Turning off banner output

--- src/main/java/com/hackorama/mcore/server/spring/Application.java
+++ src/main/java/com/hackorama/mcore/server/spring/Application.java
@@ -2,6 +2,7 @@ package com.hackorama.mcore.server.spring;
 
 import java.util.Collections;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
@@ -17,6 +18,7 @@ public class Application {
     public static void start(int port) {
         String[] args = {};
         SpringApplication app = new SpringApplication(Application.class);
+        app.setBannerMode(Banner.Mode.OFF);
         app.setDefaultProperties(Collections.singletonMap("server.port", port));
         context = app.run(args);
     }
