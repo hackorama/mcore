3800027ea572ff1f46dd986d9a8f36840aa0ece1
hackorama <kishan.thomas@gmail.com>
2019-07-05 09:01:23 -0700

    Adding session tests

--- src/test/java/com/hackorama/mcore/service/SessionTest.java
+++ src/test/java/com/hackorama/mcore/service/SessionTest.java
@@ -17,12 +17,49 @@ public class SessionTest extends ServerTest {
 
         @Override
         public void configure() {
-            GET("/test", this::test);
+            GET("/start/{name}", this::startSession);
+            GET("/session", this::inSession);
+            GET("/end", this::endSession);
+            GET("/end/invalidate", this::endSessionInvalidate);
+            GET("/end/no/invalidate", this::endSessionNoInvalidate);
         }
 
-        public Response test(Request request) {
-            return new Response("test");
+        public Response endSession(Request request) {
+            Response response = new Response("END SESSION : " + request.getSession().getAttribute("NAME"));
+            request.getSession().removeAttribute("NAME"); // Not required if invalidating, like in this case
+            request.getSession().invalidate();
+            return response;
         }
+
+        public Response endSessionInvalidate(Request request) {
+            Response response = new Response("END SESSION : " + request.getSession().getAttribute("NAME"));
+            request.getSession().invalidate();
+            return response;
+        }
+
+        public Response endSessionNoInvalidate(Request request) {
+            Response response = new Response("END SESSION : " + request.getSession().getAttribute("NAME"));
+            request.getSession().removeAttribute("NAME");
+            return response;
+        }
+
+        public Response inSession(Request request) {
+            Response response = null;
+            if (request.getSession().getAttribute("NAME") == null
+                    || request.getSession().getAttribute("NAME").toString().isEmpty()) {
+                response = new Response("NO SESSION");
+            } else {
+                response = new Response("IN SESSION : " + request.getSession().getAttribute("NAME"));
+            }
+            return response;
+        }
+
+        public Response startSession(Request request) {
+            request.getSession().setAttribute("NAME", request.getPathParams().get("name"));
+            Response response = new Response("START SESSION : " + request.getSession().getAttribute("NAME"));
+            return response;
+        }
+
     }
 
     public SessionTest(String serverType) {
@@ -30,8 +67,41 @@ public class SessionTest extends ServerTest {
     }
 
     @Test
-    public void test() throws UnirestException {
-        assertEquals("Check response body", "test", TestServer.getResponse("/test").getBody());
+    public void service_testSessions() throws UnirestException {
+        final String NAME = "mcore";
+        assertEquals("Check not in session", "NO SESSION", TestServer.getResponse("/session").getBody());
+        assertEquals("Check started session", "START SESSION : " + NAME,
+                TestServer.getResponse("/start/" + NAME).getBody());
+        assertEquals("Check in session", "IN SESSION : " + NAME, TestServer.getResponse("/session").getBody());
+        assertEquals("Check in session", "IN SESSION : " + NAME, TestServer.getResponse("/session").getBody());
+        assertEquals("Check session ended", "END SESSION : " + NAME, TestServer.getResponse("/end").getBody());
+        assertEquals("Check not in session", "NO SESSION", TestServer.getResponse("/session").getBody());
+    }
+
+    @Test
+    public void service_testSessions_withNoSessionInvalidation() throws UnirestException {
+        final String NAME = "mcore";
+        assertEquals("Check not in session", "NO SESSION", TestServer.getResponse("/session").getBody());
+        assertEquals("Check started session", "START SESSION : " + NAME,
+                TestServer.getResponse("/start/" + NAME).getBody());
+        assertEquals("Check in session", "IN SESSION : " + NAME, TestServer.getResponse("/session").getBody());
+        assertEquals("Check in session", "IN SESSION : " + NAME, TestServer.getResponse("/session").getBody());
+        assertEquals("Check session ended without invalidation", "END SESSION : " + NAME,
+                TestServer.getResponse("/end/no/invalidate").getBody());
+        assertEquals("Check not in session", "NO SESSION", TestServer.getResponse("/session").getBody());
+    }
+
+    @Test
+    public void service_testSessions_withSessionInvalidation() throws UnirestException {
+        final String NAME = "mcore";
+        assertEquals("Check not in session", "NO SESSION", TestServer.getResponse("/session").getBody());
+        assertEquals("Check started session", "START SESSION : " + NAME,
+                TestServer.getResponse("/start/" + NAME).getBody());
+        assertEquals("Check in session", "IN SESSION : " + NAME, TestServer.getResponse("/session").getBody());
+        assertEquals("Check in session", "IN SESSION : " + NAME, TestServer.getResponse("/session").getBody());
+        assertEquals("Check session ended with invalidation", "END SESSION : " + NAME,
+                TestServer.getResponse("/end/invalidate").getBody());
+        assertEquals("Check not in session", "NO SESSION", TestServer.getResponse("/session").getBody());
     }
 
     @Override
