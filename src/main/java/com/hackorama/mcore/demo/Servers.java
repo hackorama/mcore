93f0a0a515fe8d70f733bd9fa836ae12c8fcad68
hackorama <kishan.thomas@gmail.com>
2019-07-13 08:58:50 -0700

    Formatting output as table for easy comparison

--- src/main/java/com/hackorama/mcore/demo/Servers.java
+++ src/main/java/com/hackorama/mcore/demo/Servers.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
@@ -34,8 +35,22 @@ import com.hackorama.mcore.service.Service;
 public class Servers {
 
     private static class Result {
-        List<org.apache.http.cookie.Cookie> cookieResults = new ArrayList<>();
-        SortedMap<String, List<String>> headerResults = new TreeMap<>();
+        List<org.apache.http.cookie.Cookie> cookies = new ArrayList<>();
+        SortedMap<String, List<String>> headers = new TreeMap<>();
+    }
+
+    public static void addColumnCell(SortedMap<String, List<String>> table, int column, String name, String value) {
+        if (!table.containsKey(name)) {
+            table.put(name, new ArrayList<>());
+        }
+        List<String> row = table.get(name);
+        if (row.size() < column) {
+            row.add("");
+        } else if (row.size() == column) {
+            row.add(value);
+        } else if (row.size() > column) {
+            row.set(row.size() - 1, row.get(row.size() - 1) + " " + value);
+        }
     }
 
     public static void main(String[] args) throws InterruptedException, UnirestException {
@@ -44,25 +59,43 @@ public class Servers {
         results.put("SPRING", runOnServer(new SpringServer("Spring")));
         results.put("VERTX", runOnServer(new VertxServer("Vertx")));
         results.put("PLAY", runOnServer(new PlayServer("Play")));
-        System.out.println("COOKIES");
-        results.forEach((k, v) -> {
-            System.out.println("  " + k);
-            v.cookieResults.forEach(e -> {
-                if (e.getName().startsWith("REQUEST_") || e.getName().startsWith("RESPONSE_")) {
-                    System.out.println("    " + e.getName() + " = " + e.getValue());
+
+        SortedMap<String, List<String>> cookieTable = new TreeMap<>();
+        int column = 0;
+        for (Entry<String, Result> entry : results.entrySet()) {
+            addColumnCell(cookieTable, column, "COOKIE", entry.getKey());
+            for (org.apache.http.cookie.Cookie cookie : entry.getValue().cookies) {
+                if (cookie.getName().startsWith("REQUEST_") || cookie.getName().startsWith("RESPONSE_")) {
+                    addColumnCell(cookieTable, column, cookie.getName(), cookie.getValue());
                 }
-            });
-            System.out.println();
-        });
-        System.out.println("HEADERS");
-        results.forEach((k, v) -> {
-            System.out.println("  " + k);
-            v.headerResults.forEach((hk, hv) -> {
-                if (hk.startsWith("REQUEST_") || hk.startsWith("RESPONSE_")) {
-                    hv.forEach(e -> {
-                        System.out.println("    " + hk + " = " + e);
-                    });
+            }
+            column++;
+        }
+        SortedMap<String, List<String>> headerTable = new TreeMap<>();
+        column = 0;
+        for (Entry<String, Result> entry : results.entrySet()) {
+            addColumnCell(headerTable, column, "HEADER", entry.getKey());
+            for (Entry<String, List<String>> header : entry.getValue().headers.entrySet()) {
+                if (header.getKey().startsWith("REQUEST_") || header.getKey().startsWith("RESPONSE_")) {
+                    for (String value : header.getValue()) {
+                        addColumnCell(headerTable, column, header.getKey(), value);
+                    }
                 }
+            }
+            column++;
+        }
+
+        print(cookieTable);
+        System.out.println();
+        print(headerTable);
+    }
+
+    public static void print(SortedMap<String, List<String>> table) {
+        System.out.println();
+        table.forEach((k, v) -> {
+            System.out.format("| %-28s |", k);
+            v.forEach(e -> {
+                System.out.format(" %-18s |", e);
             });
             System.out.println();
         });
@@ -90,6 +123,7 @@ public class Servers {
                 response.setCookie(cookie);
                 cookie = new Cookie("RESPONSE_COOKIE_MULTI_VALUE", "SECOND");
                 cookie.setPath("/second");
+                response.setCookie(cookie);
                 cookie = new Cookie("RESPONSE_COOKIE_MULTI_VALUE", "LAST");
                 cookie.setPath("/last");
                 response.setCookie(cookie);
@@ -122,15 +156,15 @@ public class Servers {
                 .asString().getStatus() != HttpURLConnection.HTTP_OK) {
             throw new RuntimeException("Failed processing cookies correctly");
         }
-        result.cookieResults = client.getCookies();
+        result.cookies = client.getCookies();
         HttpResponse<JsonNode> response = Unirest.get("http://localhost:8080/header").header("REQUEST_ONLY", "ONE")
                 .header("REQUEST_MANY", "FIRST").header("REQUEST_MANY", "SECOND").header("REQUEST_MANY", "LAST")
                 .header("REQUEST_DUPLICATE", "SAME").header("REQUEST_DUPLICATE", "SAME").asJson();
-        result.headerResults.putAll(response.getHeaders());
+        result.headers.putAll(response.getHeaders());
         response.getBody().getObject().keySet().forEach(k -> {
-            result.headerResults.put(k, new ArrayList<>());
+            result.headers.put(k, new ArrayList<>());
             response.getBody().getObject().getJSONArray(k).forEach(e -> {
-                result.headerResults.get(k).add((String) e);
+                result.headers.get(k).add((String) e);
             });
         });
         service.stop();
