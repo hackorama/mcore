ae158d4648f0841a587d6d8f81af59b7a9798967
Kishan Thomas <kishan.thomas@gmail.com>
2019-08-15 18:54:12 -0700

    Removing unused function

--- src/main/java/m/core/common/Util.java
+++ src/main/java/m/core/common/Util.java
@@ -1,12 +1,8 @@
 package m.core.common;
 
-
-import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 public class Util {
-
-    private static final Gson GSON = new Gson();
 
     // Don't let anyone else instantiate this class
     private Util() {
@@ -15,12 +11,6 @@ public class Util {
     public static String toJsonString(String key, String value) {
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty(key, value);
-        return jsonObject.toString();
-    }
-
-    public static String toJsonString(String key, Object values) {
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty(key, GSON.toJson(values));
         return jsonObject.toString();
     }
 
