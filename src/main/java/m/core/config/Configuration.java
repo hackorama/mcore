8078c38dcc90ea63f91cc065ada20ac4838a2e7c
Kishan Thomas <kishan.thomas@gmail.com>
2019-08-19 19:40:48 -0700

    Adding javadoc comments

--- src/main/java/m/core/config/Configuration.java
+++ src/main/java/m/core/config/Configuration.java
@@ -2,6 +2,9 @@ package m.core.config;
 
 import java.util.Properties;
 
+/**
+ * A simple configuration properties manager.
+ */
 public class Configuration {
 
     private static  Properties properties = new Properties();
@@ -13,26 +16,56 @@ public class Configuration {
         properties.put("m.core.client.socket.timeout.millis", "50000");
     }
 
+    /**
+     * Returns the maximum entries a client cache can hold.
+     *
+     * @return the maximum entries a client cache can hold.
+     */
     public static int clientCacheEntriesCount() {
         return Integer.parseInt(properties.getProperty("m.core.client.cache.entries.count"));
     }
 
+    /**
+     * Returns the maximum sized object in bytes a client cache will accept.
+     *
+     * @return the maximum sized object in bytes a client cache will accept
+     */
     public static long clientCacheMaxObjectSizeBytes() {
         return Long.parseLong(properties.getProperty("m.core.client.cache.object.size.bytes"));
     }
 
+    /**
+     * Returns the connection timeout in milliseconds of a client.
+     *
+     * @return the connection timeout in milliseconds of a client
+     */
     public static int clientConnectTimeoutMillis() {
         return Integer.parseInt(properties.getProperty("m.core.client.connect.timeout.millis"));
     }
 
+    /**
+     * Returns the socket timeout in milliseconds of a client.
+     *
+     * @return the socket timeout in milliseconds of a client
+     */
     public static int clientSocketTimeoutMillis() {
         return Integer.parseInt(properties.getProperty("m.core.client.socket.timeout.millis"));
     }
 
+    /**
+     * Returns all the properties of this configuration.
+     *
+     * @return all properties
+     */
     public static Properties getProperties() {
         return Configuration.properties;
     }
 
+    /**
+     * Sets the properties of this configuration.
+     *
+     * @param properties the properties
+     */
     public static void setProperties(Properties properties) {
         Configuration.properties = properties;
     }
