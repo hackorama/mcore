0a969a46ef586bd5b5a277fdf1da6222bf35c08d
Kishan Thomas <kishan.thomas@gmail.com>
2019-08-19 19:40:12 -0700

    Adding javadoc comments

--- src/main/java/m/core/common/Util.java
+++ src/main/java/m/core/common/Util.java
@@ -2,12 +2,22 @@ package m.core.common;
 
 import com.google.gson.JsonObject;
 
+/**
+ * Common helper utilities.
+ */
 public class Util {
 
     // Don't let anyone else instantiate this class
     private Util() {
     }
 
+    /**
+     * Returns a JSON formatted string of the specified key value pair.
+     *
+     * @param key   the key name
+     * @param value the value
+     * @return the JSON string
+     */
     public static String toJsonString(String key, String value) {
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty(key, value);
