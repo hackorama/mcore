261999839e349b992a1691e5396827fdc370c9c6
Kishan Thomas <kishan.thomas@gmail.com>
2019-11-27 13:37:01 -0800

    Adding cache sample

---
+++ samples/src/main/java/m/core/samples/Cache.java
@@ -0,0 +1,29 @@
+package m.core.samples;
+
+import java.util.concurrent.TimeUnit;
+
+import m.core.data.kafka.KafkaDataQueue;
+import m.core.data.queue.DataQueue;
+import m.core.data.redis.RedisDataStoreCacheQueue;
+
+public class Cache {
+
+    public static void main(String[] args) throws InterruptedException {
+        use(new KafkaDataQueue());
+        use(new RedisDataStoreCacheQueue());
+    }
+
+    private static void use(DataQueue queue) throws InterruptedException {
+        queue.consume("test_one", Cache::consumer);
+        Thread.sleep(TimeUnit.SECONDS.toMillis(1));
+        queue.publish("test_one", "hello mcore cache");
+        queue.close();
+
+    }
+
+    public static boolean consumer(String message) {
+        System.out.println("Recived: " + message);
+        return true;
+    }
+
+}
