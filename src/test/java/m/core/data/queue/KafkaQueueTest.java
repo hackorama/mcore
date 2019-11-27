96caad82dd706c2a26c9bce33020e678cb114a56
Kishan Thomas <kishan.thomas@gmail.com>
2019-11-27 13:29:45 -0800

    Fxing tests

--- src/test/java/m/core/data/queue/KafkaQueueTest.java
+++ src/test/java/m/core/data/queue/KafkaQueueTest.java
@@ -6,7 +6,6 @@ import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -16,63 +15,65 @@ import m.core.data.kafka.KafkaDataQueue;
 public class KafkaQueueTest {
 
     private static DataQueue queue;
-    private static boolean serverIntegrationTestsIsEnabled;
     private static boolean serverConnectionIsAvailable;
+    private static boolean serverIntegrationTestsIsEnabled;
+
+    private static void closeQueues() throws InterruptedException {
+        if (queue != null) {
+            // wait for any errors from consumer threads
+            Thread.sleep(TimeUnit.SECONDS.toMillis(1));
+            queue.close();
+        }
+    }
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
         serverIntegrationTestsIsEnabled = TestService.getEnv("KAFKA_TEST");
-        serverIntegrationTestsIsEnabled = true;
-        try {
-            queue = new KafkaDataQueue();
-            serverConnectionIsAvailable = true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            if (serverIntegrationTestsIsEnabled) {
+        if (!serverIntegrationTestsIsEnabled) { // when not enabled, run tests only when there is a connection
+            System.out.println("Skipping data tests since KAFKA_TEST server is not available");
+            org.junit.Assume.assumeTrue(serverConnectionIsAvailable);
+        } else {
+            try {
+                queue = new KafkaDataQueue();
+                serverConnectionIsAvailable = true;
+            } catch (Exception e) {
                 fail("Kafka queue connection failed"); // Fail fast instead of each testing failing
+                e.printStackTrace();
             }
         }
     }
 
     @AfterClass
     public static void tearDownAfterClass() throws Exception {
-        if (queue != null) {
-            queue.close();
-        }
+        closeQueues();
     }
 
     private String receivedMessage = "";
 
     @Test
-    public void addingConsumer_ExpectsNoErrors() {
+    public void addingConsumer_ExpectsNoErrors() throws InterruptedException {
         queue.consume("test-2", this::testHandler);
     }
 
     @Test
     public void publishingMessage_ExpectsConsumingHandlerToRecieve() throws InterruptedException {
-        TimeUnit.MILLISECONDS.sleep(600); // wait for handler invocation
         receivedMessage = "";
         String publishedMessage = "hello test one";
         queue.consume("test_one", this::testHandler);
         queue.publish("test_one", publishedMessage);
-        TimeUnit.MILLISECONDS.sleep(200); // wait for handler invocation
-        assertEquals(receivedMessage, publishedMessage);
+        // wait for handler invocation and any errors from publisher/consumer threads
+        Thread.sleep(TimeUnit.SECONDS.toMillis(1));
+        assertEquals(publishedMessage, receivedMessage);
     }
 
     @Test
-    public void publishingMessage_ExpectsNoErrros() {
-        queue.publish("test-1", "hello");
-    }
-
-    @Before
-    public void setUp() throws Exception {
-        if (!serverIntegrationTestsIsEnabled) { // when not enabled, run tests only when there is a connection
-            org.junit.Assume.assumeTrue(serverConnectionIsAvailable);
-        }
+    public void publishingMessage_ExpectsNoErrros() throws InterruptedException {
+        queue.publish("test", "hello");
     }
 
     @After
     public void tearDown() throws Exception {
+        closeQueues();
     }
 
     public boolean testHandler(String message) {
