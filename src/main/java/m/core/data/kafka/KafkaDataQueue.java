765fac2c2cedcd5e9aab76cd1e87b7108106832f
Kishan Thomas <kishan.thomas@gmail.com>
2019-11-27 13:28:27 -0800

    Adding consumer closing in finally, separate properties

--- src/main/java/m/core/data/kafka/KafkaDataQueue.java
+++ src/main/java/m/core/data/kafka/KafkaDataQueue.java
@@ -23,37 +23,46 @@ import m.core.data.queue.DataQueue;
 public class KafkaDataQueue implements DataQueue {
 
     private static final String DEFAULT_BOOTSTRAP_SERVERS = "localhost:9092";
-    private static final Object DEFAULT_GROUP_ID = "MCORE_CACHE";
+    private static final Object DEFAULT_GROUP_ID = "MCORE";
     private static final Logger logger = LoggerFactory.getLogger(KafkaDataQueue.class);
     private static Map<String, ExecutorService> topicConsumerExecutors = new HashMap<>();
+    private static Map<String, Boolean> consumerLiveness = new HashMap<>();
     private static Map<String, KafkaConsumer<String, String>> topicConsumers = new HashMap<>();
 
     private KafkaProducer<String, String> kafkaProducer;
-    private Properties properties = new Properties();
+    private Properties producerProperties = new Properties();
+    private Properties consumerProperties = new Properties();
 
     public KafkaDataQueue() {
         this(DEFAULT_BOOTSTRAP_SERVERS);
     }
 
     public KafkaDataQueue(String bootstrapServers) {
-        // TODO FIXME Separate configuration for producers and consumers
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+        producerProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        producerProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
                 "org.apache.kafka.common.serialization.StringSerializer");
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+        producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
                 "org.apache.kafka.common.serialization.StringSerializer");
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+
+        consumerProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        consumerProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
                 "org.apache.kafka.common.serialization.StringDeserializer");
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+        consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
                 "org.apache.kafka.common.serialization.StringDeserializer");
-        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
-        properties.put("group.id", DEFAULT_GROUP_ID);
-        init(properties);
+        consumerProperties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        consumerProperties.put(ConsumerConfig.GROUP_ID_CONFIG, DEFAULT_GROUP_ID);
+        init(producerProperties, consumerProperties);
     }
 
     @Override
     public void close() {
+        consumerLiveness.keySet().forEach(k -> {
+            consumerLiveness.put(k, false);
+        });
+    }
 
+    public void close(String channel) {
+        consumerLiveness.put(channel, false);
     }
 
     @Override
@@ -63,29 +72,46 @@ public class KafkaDataQueue implements DataQueue {
             return;
         }
         topicConsumerExecutors.put(channel, Executors.newSingleThreadExecutor()); // TODO pooled executor
-        try {
-            topicConsumerExecutors.get(channel).submit(() -> {
-                topicConsumers.put(channel, new KafkaConsumer<String, String>(properties));
-                topicConsumers.get(channel).subscribe(Arrays.asList(channel));
-                while (true) {
-                    ConsumerRecords<String, String> consumerRecords = topicConsumers.get(channel)
-                            .poll(Duration.ofMillis(100));
-                    consumerRecords.forEach(record -> {
-                        handler.apply(record.value());
-                        topicConsumers.get(channel).commitSync();
-                    });
+        Runnable consumerTask = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    topicConsumers.put(channel, new KafkaConsumer<String, String>(consumerProperties));
+                    topicConsumers.get(channel).subscribe(Arrays.asList(channel));
+                    consumerLiveness.put(channel, true);
+                    while (consumerLiveness.get(channel)) {
+                        ConsumerRecords<String, String> consumerRecords = topicConsumers.get(channel)
+                                .poll(Duration.ofMillis(100));
+                        consumerRecords.forEach(record -> {
+                            handler.apply(record.value());
+                            topicConsumers.get(channel).commitSync();
+                        });
+                    }
+                } finally {
+                    logger.info("Closing channel {} ...", channel);
+                    if (topicConsumers.get(channel) != null) {
+                        topicConsumers.get(channel).close();
+                    } else {
+                        logger.warn("Channel {} is not valid to close", channel);
+                    }
+                    if (topicConsumerExecutors.get(channel) != null
+                            && !topicConsumerExecutors.get(channel).isShutdown()) {
+                        topicConsumerExecutors.get(channel).shutdown();
+                    } else {
+                        logger.warn("Channel {} thread executor service is already shutdown or not valid", channel);
+                    }
+
                 }
-            });
-        } finally {
-            if (topicConsumers.get(channel) != null) {
-                topicConsumers.get(channel).close();
             }
-        }
+        };
+        topicConsumerExecutors.get(channel).submit(consumerTask);
     }
 
-    public void init(Properties properties) {
-        kafkaProducer = new KafkaProducer<String, String>(properties);
-        logger.info("Connected to Kafka server: {}", properties.get("bootstrap.servers"));
+    public void init(Properties producerProperties, Properties consumerProperties) {
+        this.producerProperties = producerProperties;
+        this.consumerProperties = consumerProperties;
+        kafkaProducer = new KafkaProducer<String, String>(producerProperties);
+        logger.info("Connected to Kafka server: {}", producerProperties.get("bootstrap.servers"));
     }
 
     @Override
